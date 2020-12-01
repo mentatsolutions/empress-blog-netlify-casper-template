b262950b5a992db76d0130904d526d71ae422bc3
Wendell White <35201319+mentatsolutions@users.noreply.github.com>
2020-12-01 00:18:53 -0700

    Update environment.js

--- config/environment.js
+++ config/environment.js
@@ -29,7 +29,8 @@ module.exports = function(environment) {
       
       navigation: [
         { label: 'Home', route: 'index' }, 
-        { label: 'About', route: 'page', id: 'wendell-white' }
+        { label: 'About', route: 'page', id: 'wendell-white' },
+        { label: 'Certifications', route: 'certs', id: 'courses'}
      ]
   },
 
